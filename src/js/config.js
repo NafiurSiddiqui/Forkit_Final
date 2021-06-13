5e9112d0dc1b3574c1cad988d43eee19987f6884
TheHunter808 <saejim91@gmail.com>
2021-06-13 23:56:12 +0600

    init

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '63616dbd-c228-43bd-b922-34d597397767';
+export const MODAL_CLOSE_SEC = 2.5;
